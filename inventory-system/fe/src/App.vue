5941c70fbd364eb6d39c6283e2f6658f4cb62542
Darren Atienza <datienza@doehle-mnl.com>
2025-03-18 20:18:32 +0800

    update code

--- inventory-system/fe/src/App.vue
+++ inventory-system/fe/src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { computed, onMounted } from 'vue'
-import { useProductsStore } from './stores/products'
 import NavBar from './components/NavBar.vue'
 import SideBar from './components/SideBar.vue'
+import GlobalAlert from '@/components/GlobalAlert.vue'
 </script>
 
 <template>
   <div id="app" class="flex flex-col h-screen bg-gray-50 overflow-hidden">
+    <GlobalAlert />
     <NavBar />
     <div class="flex flex-1 overflow-hidden">
       <SideBar />
